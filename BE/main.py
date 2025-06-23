2ee82f1e5bcc285ffb47b18e14bcff339f8b922c
baominh5xx2 <baominh5xx2@gmail.com>
2025-06-23 22:21:34 +0700

    update structure of project

---
+++ BE/main.py
@@ -0,0 +1,2 @@
+# workflow -> prompt của người dùng -> đưa qua llm -> llm trả về các features ở dạng json -> lưu dữ liệu này ở dạng file json
+# -> api history sẽ dùng file json để đưa data lên frontend
